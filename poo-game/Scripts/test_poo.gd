59438ead7d6b4b41088dadb878f68cce4b94abb0
jamesbakis <s3783333@student.rmit.edu.au>
2025-06-15 23:41:10 +1000

    working moving enemies

--- poo-game/Scripts/test_poo.gd
+++ poo-game/Scripts/test_poo.gd
@@ -1,25 +1,21 @@
 extends CharacterBody2D
 
+var direction = 1
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if global_position.x > 1900:
+		direction = 0
+	elif global_position.x < 20:
+		direction = 1
+	
+	if direction == 1:
+		position.x += 5
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+		position.x -= 5 
+	
+	pass
+		
--- poo-game/player.gd
+++ poo-game/player.gd
@@ -1,22 +1,32 @@
 extends CharacterBody2D
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var jump: AudioStreamPlayer2D = $Jump
 
-const SPEED = 250
-const JUMP_VELOCITY = -400
+const SPEED = 500
+const JUMP_VELOCITY = -1200
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * delta + Vector2(0, 30)
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jump.play()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
+	
+	# Flip the Sprite
+	if direction > 0:
+		animated_sprite_2d.flip_h = false
+	elif direction < 0:
+		animated_sprite_2d.flip_h = true
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
